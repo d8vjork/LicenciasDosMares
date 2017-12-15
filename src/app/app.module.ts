b9c2f67b9c17b0d283b8bfbfcb92d917a31b5e4e
D8vjork <d8vjork@outlook.com>
2017-12-15 22:51:47 +0100

    WIP

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -20,10 +20,10 @@ import { CreateModelPage } from '../pages/create-model/create-model'
 
 import { StatusBar } from '@ionic-native/status-bar'
 import { SplashScreen } from '@ionic-native/splash-screen'
-import { File } from '@ionic-native/file';
-import { Transfer } from '@ionic-native/transfer';
-import { FilePath } from '@ionic-native/file-path';
-import { Camera } from '@ionic-native/camera';
+import { File } from '@ionic-native/file'
+import { Transfer } from '@ionic-native/transfer'
+import { FilePath } from '@ionic-native/file-path'
+import { Camera } from '@ionic-native/camera'
 
 @NgModule({
   declarations: [
